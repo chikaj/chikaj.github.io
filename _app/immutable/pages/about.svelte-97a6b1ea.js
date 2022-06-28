ac859808c02c4fb940f57a89fc5d8211926a7791
chikaj <ncurrit@gmail.com>
2022-06-27 20:05:31 -0500

    Updates

---
+++ _app/immutable/pages/about.svelte-97a6b1ea.js
@@ -0,0 +1,7 @@
+import{S as u,i as v,s as m,e as c,t as y,c as d,a as p,h as f,d as i,g as _,I as h,n as r}from"../chunks/index-647bb181.js";function x(l){let e,a,n;return{c(){e=c("div"),a=c("p"),n=y(`Nate Currit is a University professor and geospatial data scientist at Texas State University. 
+        My research and teaching interests are in GIScience, with special emphasis on Earth Observation analysis, 
+        spatial databases and human-environment interaction. 
+        I choose to use open source tools and data whenever the opportunity arises.`)},l(t){e=d(t,"DIV",{});var s=p(e);a=d(s,"P",{});var o=p(a);n=f(o,`Nate Currit is a University professor and geospatial data scientist at Texas State University. 
+        My research and teaching interests are in GIScience, with special emphasis on Earth Observation analysis, 
+        spatial databases and human-environment interaction. 
+        I choose to use open source tools and data whenever the opportunity arises.`),o.forEach(i),s.forEach(i)},m(t,s){_(t,e,s),h(e,a),h(a,n)},p:r,i:r,o:r,d(t){t&&i(e)}}}class I extends u{constructor(e){super(),v(this,e,null,x,m,{})}}export{I as default};
---
+++ _app/immutable/pages/index@home.svelte-7c35a841.js
@@ -0,0 +1,73 @@
+import{S as Pe,i as Ie,s as Ce,e as s,t as r,k as l,c as u,a as n,h as c,d as t,m,b as o,g as Le,I as e,n as ce}from"../chunks/index-647bb181.js";function Ne(Ae){let p,L,y,M,i,d,O,R,v,D,S,h,V,H,q,j,w,f,z,B,x,F,G,_,J,K,b,T,U,E,W,X,g,Y,Z,Q,$,ee,k,ie,ae,A,te,se,P,ue,ne,I,re;return{c(){p=s("div"),L=s("h1"),y=r("Nate Currit's site"),M=l(),i=s("div"),d=s("p"),O=r("Learn more here!"),R=l(),v=s("p"),D=r(`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
+      adipisci sapiente, distinctio unde soluta eos perspiciatis dolore in.
+      Nihil, exercitationem! Quia quae, laudantium unde cupiditate quod dolorem
+      eveniet corporis. Recusandae.`),S=l(),h=s("p"),V=r(`Exercitationem ea voluptatum voluptatem veniam provident ratione hic
+      tenetur rem quidem, iusto assumenda, delectus nisi itaque sit reiciendis
+      mollitia asperiores molestias cum sapiente quia accusamus repellat
+      voluptatibus omnis id! Atque.`),H=l(),q=s("p"),j=r(`A neque, at amet quia id ipsa aut qui nemo nam quaerat architecto illo
+      dolor commodi? Magni veniam officiis, expedita aspernatur officia totam
+      sunt accusamus maiores cupiditate animi earum illo.`),w=l(),f=s("p"),z=r(`Consequatur nam saepe atque eum at placeat hic vero esse quasi distinctio
+      accusamus laborum id repellat quae facilis, molestiae, ex officiis aut
+      mollitia consequuntur adipisci laboriosam, perspiciatis harum. Aut, iusto.`),B=l(),x=s("p"),F=r(`Ipsa iusto nihil facere, ducimus ipsam debitis exercitationem nam tenetur.
+      Quasi fuga velit repudiandae in adipisci sint reprehenderit delectus,
+      excepturi asperiores at autem veniam nesciunt! Quaerat minima vel
+      accusantium consequuntur?`),G=l(),_=s("p"),J=r(`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
+      adipisci sapiente, distinctio unde soluta eos perspiciatis dolore in.
+      Nihil, exercitationem! Quia quae, laudantium unde cupiditate quod dolorem
+      eveniet corporis. Recusandae.`),K=l(),b=s("p"),T=r(`Exercitationem ea voluptatum voluptatem veniam provident ratione hic
+      tenetur rem quidem, iusto assumenda, delectus nisi itaque sit reiciendis
+      mollitia asperiores molestias cum sapiente quia accusamus repellat
+      voluptatibus omnis id! Atque.`),U=l(),E=s("p"),W=r(`A neque, at amet quia id ipsa aut qui nemo nam quaerat architecto illo
+      dolor commodi? Magni veniam officiis, expedita aspernatur officia totam
+      sunt accusamus maiores cupiditate animi earum illo.`),X=l(),g=s("p"),Y=r(`Consequatur nam saepe atque eum at placeat hic vero esse quasi distinctio
+      accusamus laborum id repellat quae facilis, molestiae, ex officiis aut
+      mollitia consequuntur adipisci laboriosam, perspiciatis harum. Aut, iusto.`),Z=l(),Q=s("p"),$=r(`Ipsa iusto nihil facere, ducimus ipsam debitis exercitationem nam tenetur.
+      Quasi fuga velit repudiandae in adipisci sint reprehenderit delectus,
+      excepturi asperiores at autem veniam nesciunt! Quaerat minima vel
+      accusantium consequuntur?`),ee=l(),k=s("p"),ie=r(`Consequatur nam saepe atque eum at placeat hic vero esse quasi distinctio
+      accusamus laborum id repellat quae facilis, molestiae, ex officiis aut
+      mollitia consequuntur adipisci laboriosam, perspiciatis harum. Aut, iusto.`),ae=l(),A=s("p"),te=r(`Ipsa iusto nihil facere, ducimus ipsam debitis exercitationem nam tenetur.
+      Quasi fuga velit repudiandae in adipisci sint reprehenderit delectus,
+      excepturi asperiores at autem veniam nesciunt! Quaerat minima vel
+      accusantium consequuntur?`),se=l(),P=s("p"),ue=r(`Consequatur nam saepe atque eum at placeat hic vero esse quasi distinctio
+      accusamus laborum id repellat quae facilis, molestiae, ex officiis aut
+      mollitia consequuntur adipisci laboriosam, perspiciatis harum. Aut, iusto.`),ne=l(),I=s("p"),re=r(`Ipsa iusto nihil facere, ducimus ipsam debitis exercitationem nam tenetur.
+      Quasi fuga velit repudiandae in adipisci sint reprehenderit delectus,
+      excepturi asperiores at autem veniam nesciunt! Quaerat minima vel
+      accusantium consequuntur?`),this.h()},l(N){p=u(N,"DIV",{});var C=n(p);L=u(C,"H1",{});var oe=n(L);y=c(oe,"Nate Currit's site"),oe.forEach(t),M=m(C),i=u(C,"DIV",{class:!0});var a=n(i);d=u(a,"P",{class:!0});var le=n(d);O=c(le,"Learn more here!"),le.forEach(t),R=m(a),v=u(a,"P",{class:!0});var me=n(v);D=c(me,`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
+      adipisci sapiente, distinctio unde soluta eos perspiciatis dolore in.
+      Nihil, exercitationem! Quia quae, laudantium unde cupiditate quod dolorem
+      eveniet corporis. Recusandae.`),me.forEach(t),S=m(a),h=u(a,"P",{class:!0});var pe=n(h);V=c(pe,`Exercitationem ea voluptatum voluptatem veniam provident ratione hic
+      tenetur rem quidem, iusto assumenda, delectus nisi itaque sit reiciendis
+      mollitia asperiores molestias cum sapiente quia accusamus repellat
+      voluptatibus omnis id! Atque.`),pe.forEach(t),H=m(a),q=u(a,"P",{class:!0});var de=n(q);j=c(de,`A neque, at amet quia id ipsa aut qui nemo nam quaerat architecto illo
+      dolor commodi? Magni veniam officiis, expedita aspernatur officia totam
+      sunt accusamus maiores cupiditate animi earum illo.`),de.forEach(t),w=m(a),f=u(a,"P",{class:!0});var ve=n(f);z=c(ve,`Consequatur nam saepe atque eum at placeat hic vero esse quasi distinctio
+      accusamus laborum id repellat quae facilis, molestiae, ex officiis aut
+      mollitia consequuntur adipisci laboriosam, perspiciatis harum. Aut, iusto.`),ve.forEach(t),B=m(a),x=u(a,"P",{class:!0});var he=n(x);F=c(he,`Ipsa iusto nihil facere, ducimus ipsam debitis exercitationem nam tenetur.
+      Quasi fuga velit repudiandae in adipisci sint reprehenderit delectus,
+      excepturi asperiores at autem veniam nesciunt! Quaerat minima vel
+      accusantium consequuntur?`),he.forEach(t),G=m(a),_=u(a,"P",{class:!0});var qe=n(_);J=c(qe,`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis
+      adipisci sapiente, distinctio unde soluta eos perspiciatis dolore in.
+      Nihil, exercitationem! Quia quae, laudantium unde cupiditate quod dolorem
+      eveniet corporis. Recusandae.`),qe.forEach(t),K=m(a),b=u(a,"P",{class:!0});var fe=n(b);T=c(fe,`Exercitationem ea voluptatum voluptatem veniam provident ratione hic
+      tenetur rem quidem, iusto assumenda, delectus nisi itaque sit reiciendis
+      mollitia asperiores molestias cum sapiente quia accusamus repellat
+      voluptatibus omnis id! Atque.`),fe.forEach(t),U=m(a),E=u(a,"P",{class:!0});var xe=n(E);W=c(xe,`A neque, at amet quia id ipsa aut qui nemo nam quaerat architecto illo
+      dolor commodi? Magni veniam officiis, expedita aspernatur officia totam
+      sunt accusamus maiores cupiditate animi earum illo.`),xe.forEach(t),X=m(a),g=u(a,"P",{class:!0});var _e=n(g);Y=c(_e,`Consequatur nam saepe atque eum at placeat hic vero esse quasi distinctio
+      accusamus laborum id repellat quae facilis, molestiae, ex officiis aut
+      mollitia consequuntur adipisci laboriosam, perspiciatis harum. Aut, iusto.`),_e.forEach(t),Z=m(a),Q=u(a,"P",{class:!0});var be=n(Q);$=c(be,`Ipsa iusto nihil facere, ducimus ipsam debitis exercitationem nam tenetur.
+      Quasi fuga velit repudiandae in adipisci sint reprehenderit delectus,
+      excepturi asperiores at autem veniam nesciunt! Quaerat minima vel
+      accusantium consequuntur?`),be.forEach(t),ee=m(a),k=u(a,"P",{class:!0});var Ee=n(k);ie=c(Ee,`Consequatur nam saepe atque eum at placeat hic vero esse quasi distinctio
+      accusamus laborum id repellat quae facilis, molestiae, ex officiis aut
+      mollitia consequuntur adipisci laboriosam, perspiciatis harum. Aut, iusto.`),Ee.forEach(t),ae=m(a),A=u(a,"P",{class:!0});var ge=n(A);te=c(ge,`Ipsa iusto nihil facere, ducimus ipsam debitis exercitationem nam tenetur.
+      Quasi fuga velit repudiandae in adipisci sint reprehenderit delectus,
+      excepturi asperiores at autem veniam nesciunt! Quaerat minima vel
+      accusantium consequuntur?`),ge.forEach(t),se=m(a),P=u(a,"P",{class:!0});var Qe=n(P);ue=c(Qe,`Consequatur nam saepe atque eum at placeat hic vero esse quasi distinctio
+      accusamus laborum id repellat quae facilis, molestiae, ex officiis aut
+      mollitia consequuntur adipisci laboriosam, perspiciatis harum. Aut, iusto.`),Qe.forEach(t),ne=m(a),I=u(a,"P",{class:!0});var ke=n(I);re=c(ke,`Ipsa iusto nihil facere, ducimus ipsam debitis exercitationem nam tenetur.
+      Quasi fuga velit repudiandae in adipisci sint reprehenderit delectus,
+      excepturi asperiores at autem veniam nesciunt! Quaerat minima vel
+      accusantium consequuntur?`),ke.forEach(t),a.forEach(t),C.forEach(t),this.h()},h(){o(d,"class","svelte-14ehtks"),o(v,"class","svelte-14ehtks"),o(h,"class","svelte-14ehtks"),o(q,"class","svelte-14ehtks"),o(f,"class","svelte-14ehtks"),o(x,"class","svelte-14ehtks"),o(_,"class","svelte-14ehtks"),o(b,"class","svelte-14ehtks"),o(E,"class","svelte-14ehtks"),o(g,"class","svelte-14ehtks"),o(Q,"class","svelte-14ehtks"),o(k,"class","svelte-14ehtks"),o(A,"class","svelte-14ehtks"),o(P,"class","svelte-14ehtks"),o(I,"class","svelte-14ehtks"),o(i,"class","pt-20")},m(N,C){Le(N,p,C),e(p,L),e(L,y),e(p,M),e(p,i),e(i,d),e(d,O),e(i,R),e(i,v),e(v,D),e(i,S),e(i,h),e(h,V),e(i,H),e(i,q),e(q,j),e(i,w),e(i,f),e(f,z),e(i,B),e(i,x),e(x,F),e(i,G),e(i,_),e(_,J),e(i,K),e(i,b),e(b,T),e(i,U),e(i,E),e(E,W),e(i,X),e(i,g),e(g,Y),e(i,Z),e(i,Q),e(Q,$),e(i,ee),e(i,k),e(k,ie),e(i,ae),e(i,A),e(A,te),e(i,se),e(i,P),e(P,ue),e(i,ne),e(i,I),e(I,re)},p:ce,i:ce,o:ce,d(N){N&&t(p)}}}class Me extends Pe{constructor(p){super(),Ie(this,p,null,Ne,Ce,{})}}export{Me as default};
